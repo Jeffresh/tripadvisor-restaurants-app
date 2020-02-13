5cc6db2060de82baf1f39412b75cdb7fecb3e76f
Jeff <jeff.uca@gmail.com>
2020-02-13 15:19:51 +0100

    [MOD] import name and functional notation

--- fivetenedores/app/navigations/RestaurantsStack.tsx
+++ fivetenedores/app/navigations/RestaurantsStack.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import RestaurantsScreen from '../screens/Restaurants';
+import Restaurants from '../screens/Restaurants';
 
 const RestaurantsScreenStacks = createStackNavigator();
 
-function RestaurantsStack() {
+const RestaurantsStack = () => {
   return (
     <RestaurantsScreenStacks.Navigator>
       <RestaurantsScreenStacks.Screen
         name="Restaurants"
-        component={RestaurantsScreen}
+        component={Restaurants}
         options={{
           title: 'Restaurants',
         }}
       />
     </RestaurantsScreenStacks.Navigator>
   );
-}
+};
+
 export default RestaurantsStack;
