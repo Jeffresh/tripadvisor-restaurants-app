e1fa1301b5ed76747316b72659430ecf9007c7fc
Jeff <jeff.uca@gmail.com>
2020-02-13 15:21:04 +0100

    [ADD] Top restaurant screen and stack navigation

---
+++ fivetenedores/app/navigations/TopLIstsStacks.tsx
@@ -0,0 +1,21 @@
+import {createStackNavigator} from '@react-navigation/stack';
+import TopRestaurantsScreen from '../screens/TopRestaurants';
+import React from 'react';
+
+const TopListScreenStacks = createStackNavigator();
+
+function TopRestaurants() {
+  return (
+    <TopListScreenStacks.Navigator>
+      <TopListScreenStacks.Screen
+        name="TopRestaurants"
+        component={TopRestaurantsScreen}
+        options={{
+          title: 'Top Restaurants',
+        }}
+      />
+    </TopListScreenStacks.Navigator>
+  );
+}
+
+export default TopRestaurants;
---
+++ fivetenedores/app/screens/TopRestaurants.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function TopRestaurants() {
+  return (
+    <View>
+      <Text> Top Restaurants</Text>
+    </View>
+  );
+}
