2c275008fad6b4f2f4c23b4585f850562b209a1c
Jeff <jeff.uca@gmail.com>
2020-02-17 09:21:33 +0100

    [ADD] Firebase configuration

--- fivetenedores/App.tsx
+++ fivetenedores/App.tsx
@@ -10,6 +10,7 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import Navigation from './app/navigations/Navigation';
+import {firebaseApp} from 'app/utils/FireBase';
 
 const App = () => {
   return <Navigation />;
---
+++ fivetenedores/app/utils/FireBase.js
@@ -0,0 +1,8 @@
+import firebase from 'firebase/app';
+
+//copy your firebaseConfig here
+const firebaseConfig = {
+
+};
+
+export const firebaseApp = firebase.initializeApp(firebaseConfig);
