ffb1a5b0db99a2fee411a55322865a7128dbd82e
Jeff <jeff.uca@gmail.com>
2020-02-17 09:32:36 +0100

    [MOD] refactor account screes into a folder.

--- fivetenedores/app/navigations/MyAccountStack.tsx
+++ fivetenedores/app/navigations/MyAccountStack.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import MyAccount from '../screens/MyAccount';
+import MyAccount from '../screens/Account/MyAccount';
 
 const MyAccountScreenStacks = createStackNavigator();
 
