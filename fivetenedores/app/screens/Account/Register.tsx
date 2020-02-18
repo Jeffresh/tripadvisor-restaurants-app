fdeccde8f945149f3b6deeb7fa7a4495694d5783
Jeff <jeff.uca@gmail.com>
2020-02-18 12:29:40 +0100

    [ADD] base register screen

---
+++ fivetenedores/app/screens/Account/Register.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Register = () => {
+  return(
+    <View>
+      <Text>
+        User register
+      </Text>
+    </View>
+
+  );
+};
+
+export default Register;
