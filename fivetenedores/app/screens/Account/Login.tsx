33df150245c50be3d09c826adb89c70c71a288fe
Jeff <jeff.uca@gmail.com>
2020-02-18 12:26:47 +0100

    [MOD] added scrollview [MOD] added login and create account text [MOD] added divider [ADD] viewcontainer and divider styles

--- fivetenedores/app/screens/Account/Login.tsx
+++ fivetenedores/app/screens/Account/Login.tsx
@@ -4,12 +4,23 @@ import {Divider} from 'react-native-elements';
 import {useNavigation} from '@react-navigation/native';
 
 const Login = () => {
+  // @ts-ignore
   return (
-    <Image
-      source={require('../../../assets/5forks-logo.png')}
-      style={styles.logo}
-      resizeMode="contain"
-    />
+    <ScrollView>
+      <Image
+        source={require('../../../assets/5forks-logo.png')}
+        style={styles.logo}
+        resizeMode="contain"
+      />
+      <View style={styles.viewContainer}>
+        <Text>Form Login.. </Text>
+        <Text>Create Account.. </Text>
+      </View>
+      <Divider style={styles.divider} />
+      <View style={styles.viewContainer}>
+        <Text>Login Facebook... </Text>
+      </View>
+    </ScrollView>
   );
 };
 
@@ -18,6 +29,14 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 150,
     marginTop: 20,
+  },
+  viewContainer: {
+    marginRight: 40,
+    marginLeft: 40,
+  },
+  divider: {
+    backgroundColor: '#00a680',
+    margin: 40,
   },
 });
 
