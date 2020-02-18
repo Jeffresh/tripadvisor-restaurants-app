bdec11c0c0ede5a40780bfacb525798a6040b269
Jeff <jeff.uca@gmail.com>
2020-02-18 12:01:44 +0100

    [MOD] add logo and style sheet to login screen

--- fivetenedores/app/screens/Account/Login.tsx
+++ fivetenedores/app/screens/Account/Login.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import {StyleSheet, View, ScrollView, Text, Imagen} from 'react-native';
+import {StyleSheet, View, ScrollView, Text, Image} from 'react-native';
+import {Divider} from 'react-native-elements';
+import {useNavigation} from '@react-navigation/native';
 
 const Login = () => {
   return (
-    <View>
-      <Text>Form login...</Text>
-    </View>
+    <Image
+      source={require('../../../assets/5forks-logo.png')}
+      style={styles.logo}
+      resizeMode="contain"
+    />
   );
 };
+
+const styles = StyleSheet.create({
+  logo: {
+    width: '100%',
+    height: 150,
+    marginTop: 20,
+  },
+});
 
 export default Login;
