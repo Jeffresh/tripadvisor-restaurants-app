e27164a25aa7802eee368653959ed2cf94bfaf44
Jeff <jeff.uca@gmail.com>
2020-02-18 08:48:23 +0100

    [ADD] User guest screen

---
+++ fivetenedores/app/screens/Account/UserGuest.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const UserGuest = () => {
+  return (
+    <View>
+      <Text>UserGuest...</Text>
+    </View>
+  );
+};
+
+export default UserGuest;
