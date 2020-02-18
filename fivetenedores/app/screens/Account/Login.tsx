bac323c279feccedd83e1bfac80cb0365ccfa3eb
Jeff <jeff.uca@gmail.com>
2020-02-18 16:05:12 +0100

    [ADD] create account screen [ADD] create account navigation [ADD] create account styles

--- fivetenedores/app/screens/Account/Login.tsx
+++ fivetenedores/app/screens/Account/Login.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import {StyleSheet, View, ScrollView, Text, Image} from 'react-native';
 import {Divider} from 'react-native-elements';
+import {useNavigation} from '@react-navigation/native';
 
 const Login = () => {
+  const navigation = useNavigation();
+
   return (
     <ScrollView>
       <Image
@@ -12,7 +15,7 @@ const Login = () => {
       />
       <View style={styles.viewContainer}>
         <Text>Form Login.. </Text>
-        <Text>Create Account.. </Text>
+        <CreateAccount navigation={navigation} />
       </View>
       <Divider style={styles.divider} />
       <View style={styles.viewContainer}>
@@ -21,6 +24,21 @@ const Login = () => {
     </ScrollView>
   );
 };
+
+const CreateAccount = (props: {navigation: any}) => {
+  const {navigation} = props;
+  return (
+    <Text style={styles.textRegister}>
+      No account? create one!{' '}
+      <Text
+        style={styles.buttonRegister}
+        onPress={() => navigation.navigate('Register')}>
+        Sing up
+      </Text>
+    </Text>
+  );
+};
+
 
 const styles = StyleSheet.create({
   logo: {
@@ -31,6 +49,15 @@ const styles = StyleSheet.create({
   viewContainer: {
     marginRight: 40,
     marginLeft: 40,
+  },
+  textRegister: {
+    marginTop: 15,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  buttonRegister: {
+    color: '#00a680',
+    fontWeight: 'bold',
   },
   divider: {
     backgroundColor: '#00a680',
