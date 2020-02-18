dcd1826a1835f6e59b4938b6c5784bf09a8a4917
Jeff <jeff.uca@gmail.com>
2020-02-18 11:38:13 +0100

    [ADD] Login screen

---
+++ fivetenedores/app/screens/Account/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, View, ScrollView, Text, Imagen} from 'react-native';
+
+const Login = () => {
+  return (
+    <View>
+      <Text>Form login...</Text>
+    </View>
+  );
+};
+
+export default Login;
