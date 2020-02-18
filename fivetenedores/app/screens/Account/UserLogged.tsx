20d136a10c9f0247cf3463dbe4106ad486b45d49
Jeff <jeff.uca@gmail.com>
2020-02-18 08:54:29 +0100

    [ADD] User logged screen

---
+++ fivetenedores/app/screens/Account/UserLogged.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const UserLogged = () => {
+  return (
+    <View>
+      <Text>UserLogged...</Text>
+    </View>
+  );
+};
+export default UserLogged;
