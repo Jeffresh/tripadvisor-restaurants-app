6fb3b41eb41aa9bbf8a375b0e5088121cfc98677
Jeff <jeff.uca@gmail.com>
2020-02-19 14:36:03 +0100

    [ADD] Structure of register screen

--- fivetenedores/app/screens/Account/Register.tsx
+++ fivetenedores/app/screens/Account/Register.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
 const Register = () => {
   return (
-    <View>
-      <Text>User register</Text>
-    </View>
+    <KeyboardAwareScrollView>
+      <Image
+        source={require('../../../assets/5forks-logo.png')}
+        style={styles.logo}
+        resizeMode="contain"
+      />
+      <View style={styles.viewForm}>
+        <Text> Form Register</Text>
+      </View>
+    </KeyboardAwareScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  logo: {
+    width: '100%',
+    height: 150,
+    marginTop: 20,
+  },
+  viewForm: {
+    marginRight: 40,
+    marginLeft: 40,
+  }
+
+});
 
 export default Register;
