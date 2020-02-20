8cd67469ea992657758b345e16132278b88510cc
Jeff <jeff.uca@gmail.com>
2020-02-20 21:00:09 +0100

    [ADD] Login Facebook component

--- fivetenedores/app/components/Account/LoginFacebook.tsx
+++ fivetenedores/app/components/Account/LoginFacebook.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SocialIcon} from 'react-native-elements';
+import {FacebookApi} from '../../utils/Social';
+import * as FaceBook from 'expo-facebook';
+import * as firebase from 'firebase';
 
 const LoginFacebook = () => {
-  const login = () => {
-    console.log('Signing in with Facebook');
+  const login = async () => {
+    try {
+      await FaceBook.initializeAsync(
+        FacebookApi.application_id,
+        FacebookApi.app_name,
+      );
+      const {type, token} = await FaceBook.logInWithReadPermissionsAsync({
+        permissions: FacebookApi.permissions,
+      });
+
+      if (type === 'success') {
+        const credentials = firebase.auth.FacebookAuthProvider.credential(
+          token,
+        );
+        try {
+          await firebase.auth().signInWithCredential(credentials);
+          console.log('correct login');
+        } catch {
+          console.log('Error: cannot connect with Facebook');
+        }
+      } else if (type === 'cancel') {
+        console.log('Sign in canceled');
+      }
+    } catch {
+      console.log('Error: unknown try later');
+    }
   };
+
   return (
     <SocialIcon
       title="Sign in with Facebook"
