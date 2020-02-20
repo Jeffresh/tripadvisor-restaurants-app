1b96e7521ebeb1b643affcb5e7f5f7ee419829f9
Jeff <jeff.uca@gmail.com>
2020-02-20 08:56:36 +0100

    [ADD] button to exit session from user logged screen

--- fivetenedores/app/screens/Account/UserLogged.tsx
+++ fivetenedores/app/screens/Account/UserLogged.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {Button} from 'react-native-elements';
+import * as firebase from 'firebase';
 
 const UserLogged = () => {
   return (
     <View>
       <Text>UserLogged...</Text>
+      <Button title="Close session" onPress={() => firebase.auth().signOut()} />
     </View>
   );
 };
