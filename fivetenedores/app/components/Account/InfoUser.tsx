6c5a3034b6178d14097058d91465939afcda247c
Jeff <jeff.uca@gmail.com>
2020-02-21 09:46:39 +0100

    [ADD] get infoUser from firebase in infoUser [ADD] infoUser screen

---
+++ fivetenedores/app/components/Account/InfoUser.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import {Avatar} from 'react-native-elements';
+
+const InfoUser = (props) => {
+  const {userInfo} = props;
+  return (
+    <View>
+      <Text> UserLogged..</Text>
+    </View>
+  );
+};
+
+export default InfoUser;
--- fivetenedores/app/screens/Account/UserLogged.tsx
+++ fivetenedores/app/screens/Account/UserLogged.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text} from 'react-native';
 import {Button} from 'react-native-elements';
 import * as firebase from 'firebase';
+import InfoUser from '../../components/Account/InfoUser';
 
 const UserLogged = () => {
+  const [userInfo, setUserInfo] = useState({});
+  useEffect(() => {
+    (async () => {
+      const user = await firebase.auth().currentUser;
+      setUserInfo(user.providerData[0]);
+    })();
+  }, []);
+
   return (
     <View>
-      <Text>UserLogged...</Text>
+      <InfoUser userInfo={userInfo} />
       <Button title="Close session" onPress={() => firebase.auth().signOut()} />
     </View>
   );
