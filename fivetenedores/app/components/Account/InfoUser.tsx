c5921b93decbebe96b68863a3f85330837c43f45
Jeff <jeff.uca@gmail.com>
2020-02-24 11:13:13 +0100

    [ADD] permission to gallery

--- fivetenedores/app/components/Account/InfoUser.tsx
+++ fivetenedores/app/components/Account/InfoUser.tsx
@@ -1,15 +1,38 @@
 import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 import {Avatar} from 'react-native-elements';
+import * as firebase from 'firebase';
+import * as Permissions from 'expo-permissions';
+import * as ImagePicker from 'expo-image-picker';
 
-const InfoUser = props => {
-  const {
-    userInfo: {uid, displayName, email, photoURL},
-  } = props;
-  const changeAvatar = () => {
-    console.log('updating avatar');
+
+const InfoUser = (props) =>{
+  const{userInfo, userInfo:{ uid, displayName, email, photoURL}} = props;
+  const changeAvatar = async () => {
+    const resultPermissions = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    const resultPermissionCamera = resultPermissions.permissions.cameraRoll.status;
+
+    if(resultPermissionCamera === "denied"){
+      console.log("permissions not accepted")
+    } else{
+      const result = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        aspect: [4,3]
+      });
+
+      if(result.cancelled) {
+        console.log("images gallery closed");
+      } else {
+        uploadImage(result.uri, uid);
+      }
+    }
   };
-  return (
+
+  const uploadImage = (uri, namedImage) => {
+    console.log(`URI ${uri}`);
+    console.log(`namedImage ${namedImage}`)
+  };
+  return(
     <View style={styles.viewUserInfo}>
       <Avatar
         rounded
@@ -20,14 +43,14 @@ const InfoUser = props => {
         source={{
           uri: photoURL
             ? photoURL
-            : 'https://api.adorable.io/avatars/266/abott@adorable.png',
+            : "https://api.adorable.io/avatars/266/abott@adorable.png"
         }}
       />
       <View>
         <Text style={styles.displayName}>
-          {displayName ? displayName : 'Anonymous'}
+          {displayName ? displayName : "Anonymous"}
         </Text>
-        <Text>{email ? email : 'Social Login'}</Text>
+        <Text>{email ? email : "Social Login"}</Text>
       </View>
     </View>
   );
@@ -35,10 +58,10 @@ const InfoUser = props => {
 
 const styles = StyleSheet.create({
   viewUserInfo: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row',
-    backgroundColor: '#f2f2f2',
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    backgroundColor: "#f2f2f2",
     paddingTop: 30,
     paddingBottom: 30,
   },
@@ -46,8 +69,10 @@ const styles = StyleSheet.create({
     marginRight: 20,
   },
   displayName: {
-    fontWeight: 'bold',
-  },
+    fontWeight: "bold",
+  }
+
 });
 
 export default InfoUser;
+
