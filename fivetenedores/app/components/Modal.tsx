6b3d7b6acd95df3907d7ce0177f3ca4d741887f7
Jeff <jeff.uca@gmail.com>
2020-02-24 14:56:05 +0100

    [ADD] modal component

---
+++ fivetenedores/app/components/Modal.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {Overlay} from 'react-native-elements';
+
+const styles = StyleSheet.create({
+  overlay : {
+    height: "auto",
+    width: "90%",
+    backgroundColor: "#fff",
+  },
+});
+const Modal = (props) => {
+  const{isVisible, setIsVisible, children} = props;
+  const closeModal = () => setIsVisible(false);
+  return (
+    <Overlay
+      isVisible={isVisible}
+      windowBackgroundColor='rgba(0,0,0, .5)'
+      overlayBackgroundColor='transparent'
+      overlayStyle={styles.overlay}
+      onBackdropPress={closeModal}
+      >
+      {children}
+
+    </ Overlay>
+  );
+};
+export default Modal;
