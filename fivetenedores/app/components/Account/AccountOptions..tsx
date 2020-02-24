ad82e0268031170aab57c466afa0872f34ab63c5
Jeff <jeff.uca@gmail.com>
2020-02-24 14:09:42 +0100

    [ADD] Account option base component

---
+++ fivetenedores/app/components/Account/AccountOptions..tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import {ListItem} from 'react-native-elements';
+
+const AccountOptions = () =>{
+  return(
+    <View>
+      <Text>Account Options</Text>
+    </View>
+  );
+};
+
+
+export default AccountOptions;
