23c2698a257be4662b581b4f51828f21a4710cd6
Jeff <jeff.uca@gmail.com>
2020-02-25 09:32:29 +0100

    [MOD] structure and styles of modal change name

--- fivetenedores/app/components/Account/ChangeDisplayName.tsx
+++ fivetenedores/app/components/Account/ChangeDisplayName.tsx
@@ -1,11 +1,55 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import {Button, Input} from 'react-native-elements';
+
+const styles = StyleSheet.create({
+  view:{
+    alignItems: "center",
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  input: {
+    marginBottom: 10
+  },
+  buttonContainer: {
+    marginTop: 20,
+  },
+  button: {
+    backgroundColor: "#00a680"
+  }
+});
 
 const ChangeDisplayNameForm = () => {
+
+  const updateDisplayName = () =>{
+    console.log("Name updated")
+
+  };
+
   return(
-    <View>
-      <Text> Change DisplayName</Text>
+    <View style={styles.view}>
+      <Input
+        placeholder='Name'
+        containerStyle={styles.input}
+        // defaultValue={}
+        // onChange={}
+        rightIcon={{
+          type: "material-community",
+          name: "account-circle-outline",
+          color: "#c2c2c2",
+        }}
+        // errorMessage={}
+      />
+      <Button
+        title="Change name"
+        containerStyle={styles.buttonContainer}
+        buttonStyle={styles.button}
+        onPress={updateDisplayName}
+        // loading={}
+      />
+
     </View>
+
   );
 };
 
