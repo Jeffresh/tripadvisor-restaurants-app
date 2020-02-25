79fe7ae138ade7b6913556fdd97d2b5cfdebab30
Jeff <jeff.uca@gmail.com>
2020-02-25 13:09:37 +0100

    [ADD] structure needed to add logic to update password

--- fivetenedores/app/components/Account/ChangePasswordForm.tsx
+++ fivetenedores/app/components/Account/ChangePasswordForm.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, Text} from 'react-native';
 import {Input, Button} from 'react-native-elements';
 import * as firebase from 'firebase';
 import reauthenticate from '../../utils/Api';
@@ -39,21 +39,56 @@ const ChangePasswordForm = (props) => {
   return(
     <View style={styles.view}>
       <Input
-        placeholder='Password'
+        placeholder="Password"
         containerStyle={styles.input}
-        // defaultValue={}
-        // onChange={}
+        password={true}
+        secureTextEntry={hidePassword}
+        onChange={e => setPassword(e.nativeEvent.text)}
         rightIcon={{
           type: "material-community",
-          name: "account-circle-outline",
+          name: hidePassword ? "eye-outline": "eye-off-outline",
           color: "#c2c2c2",
+          onPress: () => setHidePassword(!hidePassword),
         }}
-        // errorMessage={}
+        errorMessage={error.password}
       />
+      <Input
+        placeholder="New Password"
+        containerStyle={styles.input}
+        password={true}
+        secureTextEntry={hideNewPassword}
+        onChange={e => setNewPassword(e.nativeEvent.text)}
+        rightIcon={{
+          type: "material-community",
+          name: hideNewPassword ? "eye-outline" : "eye-off-outline",
+          color: "#c2c2c2",
+          onPress: () => setHideNewPassword(!hideNewPassword)
+        }}
+        errorMessage={error.newPassword}
+      />
+      <Input
+        placeholder="Repeat new Password"
+        containerStyle={styles.input}
+        password={true}
+        secureTextEntry={hideNewPasswordRepeat}
+        onChange={e => setNewPasswordRepeat(e.nativeEvent.text)}
+        rightIcon={{
+          type: "material-community",
+          name: hideNewPasswordRepeat ? "eye-outline" : "eye-off-outline",
+          color: "#c2c2c2",
+          onPress: () => setHideNewPasswordRepeat(!hideNewPasswordRepeat)
+        }}
+        errorMessage={error.newPasswordRepeat}
+        />
       <Button
         title="Change password"
         containerStyle={styles.buttonContainer}
-        />
+        buttonStyle={styles.button}
+        onPress={updatePassword}
+        loading={isLoading}
+      />
+
+      <Text>{error.general}</Text>
 
     </View>
   );
