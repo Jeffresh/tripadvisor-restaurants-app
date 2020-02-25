e6f5bfb199774bfeb07ef7be7b44490286b0bda0
Jeff <jeff.uca@gmail.com>
2020-02-25 12:33:54 +0100

    [ADD] create states necessaries to Change password form

--- fivetenedores/app/components/Account/ChangePasswordForm.tsx
+++ fivetenedores/app/components/Account/ChangePasswordForm.tsx
@@ -1,15 +1,62 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {Input, Button} from 'react-native-elements';
+import * as firebase from 'firebase';
+import reauthenticate from '../../utils/Api';
 
-const ChangePasswordForm = () => {
+const styles = StyleSheet.create({
+  view:{
+    alignItems: "center",
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  input: {
+    marginBottom: 10
+  },
+  buttonContainer: {
+    marginTop: 20,
+  },
+  button: {
+    backgroundColor: "#00a680"
+  }
+});
+
+const ChangePasswordForm = (props) => {
+  const {setIsVisibleModal, toastRef} = props;
+  const [password, setPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [newPasswordRepeat, setNewPasswordRepeat] = useState("");
+  const [error, setError] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [hidePassword, setHidePassword] = useState(true);
+  const [hideNewPassword, setHideNewPassword] = useState(true);
+  const [hideNewPasswordRepeat, setHideNewPasswordRepeat] = useState(true);
+
+  const updatePassword = () => {
+    console.log("password updated");
+  };
+
   return(
-    <View>
-      <Text>
-        Change Password
-      </Text>
+    <View style={styles.view}>
+      <Input
+        placeholder='Password'
+        containerStyle={styles.input}
+        // defaultValue={}
+        // onChange={}
+        rightIcon={{
+          type: "material-community",
+          name: "account-circle-outline",
+          color: "#c2c2c2",
+        }}
+        // errorMessage={}
+      />
+      <Button
+        title="Change password"
+        containerStyle={styles.buttonContainer}
+        />
+
     </View>
   );
 };
 
 export default  ChangePasswordForm
-
