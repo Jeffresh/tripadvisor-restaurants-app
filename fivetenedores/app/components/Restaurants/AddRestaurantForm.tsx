30bc085122b802c2624c9f29a2f65797e885f4fc
Jeff <jeff.uca@gmail.com>
2020-02-26 10:17:06 +0100

    [ADD] base addRestaurants form

---
+++ fivetenedores/app/components/Restaurants/AddRestaurantForm.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const AddRestaurantsForm = (props) => {
+  return (
+    <View>
+      <Text>AddRestaurant form</Text>
+    </View>
+
+  )
+};
+
+export default AddRestaurantsForm;
