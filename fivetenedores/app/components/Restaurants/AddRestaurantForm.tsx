3b16abfdb0e69f577b76809e5211fbc689e7d5ba
Jeff <jeff.uca@gmail.com>
2020-02-26 11:21:09 +0100

    [ADD] logic to upload images in add restaurant form

--- fivetenedores/app/components/Restaurants/AddRestaurantForm.tsx
+++ fivetenedores/app/components/Restaurants/AddRestaurantForm.tsx
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import {StyleSheet, View, ScrollView, Alert, Dimensions} from 'react-native';
 import {Icon, Avatar, Image, Input, Button} from 'react-native-elements';
+import * as Permissions from 'expo-permissions';
+import * as ImagePicker from 'expo-image-picker';
 
 const styles = StyleSheet.create({
   viewImages: {
@@ -33,15 +35,37 @@ const AddRestaurantsForm = (props) => {
   return (
     <ScrollView>
       <UploadImage
-        imageSelect={imagesSelected}
+        imagesSelect={imagesSelected}
         setImagesSelected={setImagesSelected}
+        toastRef={toastRef}
       />
     </ScrollView>
   )
 };
 
 const UploadImage = (props) => {
-  const {imageSelected, setImageSelected} = props;
+  const {imagesSelected, setImagesSelected, toastRef} = props;
+  const imageSelect = async  () => {
+    const resultPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    const resultPermissionCamera = resultPermission.permissions.cameraRoll.status;
+    if (resultPermissionCamera === "denied") {
+      toastRef.current.show("You must accept the gallery permissions", 1000)
+    }
+    else {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        aspect: [4, 3]
+      });
+      if(result.cancelled) {
+        toastRef.current.show(
+          "You closed the gallery without select a image"
+        );
+      }
+      else {
+        setImagesSelected([...imagesSelected, result.uri]);
+      }
+    }
+  };
   return (
     <View style={styles.viewImages}>
       <Icon
@@ -49,7 +73,7 @@ const UploadImage = (props) => {
         type="material-community"
         color="#7a7a7a"
         containerStyle={styles.containerIcon}
-        onPress={() => console.log("Uploading image ...")}
+        onPress={imageSelect}
       />
       <Avatar
         onPress={() => console.log("Deleting image")}
